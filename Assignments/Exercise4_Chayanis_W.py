c0cdde9127cc345acb6b1a56fd07e904306d8fd2
ChayanisWongs <chayanis.wongs@gmail.com>
2019-07-24 18:08:26 +0700

    Added Exercise4 File
    Exercise 4 : ปฏิบัติการทดสอบการใช้งานตัวแปร

---
+++ Assignments/Exercise4_Chayanis_W.py
@@ -0,0 +1,12 @@
+eng = 80
+busi = 85
+sys = 90
+prog = 95
+print("---------------------------------------")
+print("          --- Your Score ---")
+print("---------------------------------------")
+print("Foundation English : ", eng)
+print("General Business : ", busi)
+print("Introduction to Computer Systems : ", sys)
+print("Computer Programming : ", prog)
+print("---------------------------------------")
