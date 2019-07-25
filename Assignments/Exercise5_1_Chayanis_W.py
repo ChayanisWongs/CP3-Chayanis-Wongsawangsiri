10abc1c1114392352cb16f0cf646aecfd6d1587e
ChayanisWongs <chayanis.wongs@gmail.com>
2019-07-25 17:46:28 +0700

    Added Exercise5_1 File
    Exercise 5 : ปฏิบัติการพัฒนาโปรแกรมที่ใช้งานได้จริง และ ข้อมูล (ตัวดำเนินการ)

---
+++ Assignments/Exercise5_1_Chayanis_W.py
@@ -0,0 +1,15 @@
+print("-----------------------------")
+firstNum = int(input("Input Your First Number : "))
+secondNum = int(input("Input Your Second Number : "))
+print("-----------------------------")
+result1 = firstNum + secondNum
+result2 = firstNum - secondNum
+result3 = firstNum * secondNum
+result4 = int(firstNum / secondNum)
+print("    ---- Your Result ----   ")
+print("-----------------------------")
+print(firstNum,"+",secondNum,"=",result1)
+print(firstNum,"-",secondNum,"=",result2)
+print(firstNum,"*",secondNum,"=",result3)
+print(firstNum,"/",secondNum,"=",result4)
+print("-----------------------------")
